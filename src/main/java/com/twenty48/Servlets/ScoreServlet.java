8366954de593ebd777c63ad9949b25db67f26263
Cherepanov Roman <romach007@gmail.com>
2015-07-16 21:13:25 +0300

    add score updating on the main page

---
+++ src/main/java/com/twenty48/Servlets/ScoreServlet.java
@@ -0,0 +1,39 @@
+package com.twenty48.Servlets;
+
+import com.twenty48.Classes.Game;
+import com.twenty48.Classes.Score;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ScoreServlet extends HttpServlet {
+    public static final long serialVersionID = 1L;
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        HttpSession session = request.getSession(true); // if there's no session object, the one will be created
+        session.setMaxInactiveInterval(30*60); // inactive lifetime of session object (in seconds)
+        Game currentGame = (Game)session.getAttribute("game");
+
+        if(currentGame == null){
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/newgame");
+            dispatcher.forward(request, response);
+        }
+        else {
+            int currentScore = Score.getCurrentScore();
+            String JSONtiles = currentGame.getTiles();
+            PrintWriter out = response.getWriter();
+            out.println(currentScore);
+            out.close();
+        }
+    }
+}
